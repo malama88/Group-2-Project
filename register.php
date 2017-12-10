18e1ba8426ea911d231b55d468985b0d7369dd58
Nichole Rich <nrich3393@users.noreply.github.com>
2017-12-10 16:27:58 -0500

    file did not add last time

--- register.php
+++ register.php
@@ -13,38 +13,62 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $problem = false; // No problems so far.
 
     // Check for each value...
-    if (empty($_POST['first_name'])) {
+    $first_name=$_POST['first_name'];
+    if (empty($first_name)) {
         $problem = true;
         print '<p class="text--error">Please enter your first name!</p>';
     }
 
-    if (empty($_POST['last_name'])) {
+    $last_name=$_POST['last_name'];
+    if (empty($last_name)) {
         $problem = true;
         print '<p class="text--error">Please enter your last name!</p>';
     }
 
-    if (empty($_POST['email']) || (substr_count($_POST['email'], '@') != 1) ) {
+    $email=$_POST['email'];
+    if (empty($email) || (substr_count($email, '@') != 1) ) {
         $problem = true;
         print '<p class="text--error">Please enter your email address!</p>';
     }
 
-    $email=$_POST['email'];
+
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
         echo("$email is a valid email address");
     } else {
         echo("$email is not a valid email address");
     }
 
-    if (empty($_POST['password1'])) {
+    $password1=$_POST['password1'];
+    if (empty($password1)) {
         $problem = true;
         print '<p class="text--error">Please enter a password!</p>';
     }
 
 
+
     if (!$problem) { // If there weren't any problems...
 
-        // Print a message:
-        print '<p class="text--success">You are now registered!</p>';
+        // Connect
+        $dbc = mysqli_connect('thewritedev.com', 'thewrjk1_group', 'web2310', 'thewrjk1_WEB2310');
+
+        // Check to see if user is registered
+
+
+
+        // Define the query
+        $query = "INSERT INTO USERS (first_name, last_name, email, password, signup_date) 
+          VALUES ('$first_name', '$last_name', '$email', '$password1', NOW())";
+
+        // Execute the query
+        if (@mysqli_query($dbc, $query)) {
+            print '<p class="text--success">You are now registered!</p>';
+        } else {
+            print '<p>Could not register because: <br>' . mysqli_error($dbc) . '.</p>
+                <p>The query being run was: ' .$query . '</p>';
+        }
+
+        mysqli_close($dbc); // close the connection
+
 
         // Send the email:
         //$body = "Thank you, {$_POST['first_name']}, for registering with My Online Journal!'.";
@@ -74,14 +98,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <p><input placeholder= "Create Password" type="text" name="password1" size="20" required value="<?php if (isset($_POST['password1'])) { print htmlspecialchars($_POST['password1']); } ?>"></p>
 
 
-        <p>Reason for using the online journal: <br>
-            <input type="radio" name="response" value="personal" required> personal <br>
-            <input type="radio" name="response" value="business"> business <br>
-            <input type="radio" name="response" value="both"> both</p>
-
-
         <input type="submit" name="submit" value="Submit">
 
     </form>
+
+<?php
+
+?>
 
 <?php include('templates/footer.html'); // Need the footer. ?>
