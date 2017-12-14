5556d3d0c25649e8ff98b5ae24979dd1fca7418e
Nichole Rich <nrich3393@users.noreply.github.com>
2017-12-14 13:54:23 -0500

    created welcome.php

---
+++ welcome.php
@@ -0,0 +1,12 @@
+<?php
+
+define('TITLE', 'Welcome');
+include('templates/header.html');
+
+// Print some introductory text:
+print '<h2>Welcome to My Online Journal</h2>
+    <p>Get started by creating your first journal entry.</p>';
+
+
+
+include('templates/footer.html'); // Need the footer. ?>
